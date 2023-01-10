ded25451168e2b134521754d3d98fc1a853bdced
Alourne <120846738+Alourne@users.noreply.github.com>
2023-01-10 09:25:07 -0500

    finished 01/02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    var word = '';
+    while (times > 0) {
+      word += string;
+     times --;
+    }
+    return word;
 };
 
 // Do not edit below this line

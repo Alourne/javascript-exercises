8322669b017627221bff87e9462edf4e66686247
Alourne <120846738+Alourne@users.noreply.github.com>
2023-01-16 23:47:14 -0500

    .

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(y, x) {
+    let error = 'ERORR';
+    let final = 0
 
+    let x = Math.max(num1, num2);
+    let y = Math.min (num1, num2);
+
+    if ((num1 < 0 ) && (num2 < 0)) {
+        return error;
+    }
+
+    if (y > x) {
+        const temp = y;
+        x = y;
+        y = temp;
+    }
+    
+    for (let i = y; i < x + 1; i++) {
+        final = final + i;
+    }
+    return final;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,26 @@
-const leapYears = function() {
+const leapYears = function(x) {
+    let years;
+    if ((( x % 4 === 0) && (x % 100 !==0) || (x % 400 === 0 ))) {
+        years = true;
+    }
+    else {
+        years = false;
+    }
+return years;
 
+
+
+    // my solution
+    // let leap = (x);
+    // let yes = "true";
+    // let no = "false";
+
+    // if ((leap / 4) && (leap / 400)) {
+    //     return yes;
+    // }
+    // else {
+    //     no
+    // }
 };
 
 // Do not edit below this line

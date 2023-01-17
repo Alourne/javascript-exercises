9a26a006d9ac89a17c433ba44042b20aba11834b
Alourne <120846738+Alourne@users.noreply.github.com>
2023-01-16 23:56:22 -0500

    did temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let celsius = ((F-32) * 5/9);
+  let rounded = Math.round(celsius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let fahrenheit = ((C*9/5) + 32);
+  let fRounded = Math.round(fahrenheit * 10) / 10;
+  return fRounded;
 };
 
 // Do not edit below this line
